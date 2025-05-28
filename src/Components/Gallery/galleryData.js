de56634c93df9484b42e8bbefc0b3de45f972d0a
adityawintech <aditya#19AI>
2025-05-28 09:07:37 +0530

    Aditya

--- src/Components/Gallery/galleryData.js
+++ src/Components/Gallery/galleryData.js
@@ -1,4 +1,42 @@
 export const galleryData = [
+  
+  {
+    id: 3,
+    category: "Deep Tissue",
+    title: "Muscle Tension Relief",
+    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397886/img_9_nhqusz.jpg?height=400&width=600",
+  },
+   {
+    id: 8,
+        category: "Cupping Therapy",
+    title: "Wet Cupping",
+    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397889/img_14_y4fkr2.webp?height=400&width=600",
+  },
+ 
+  {
+    id: 6,
+         category: "Cupping Therapy",
+    title: "Fire Cupping",
+    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397870/img_1_a3qyvb.webp?height=400&width=600",
+  },
+    {
+    id: 5,
+      category: "Cupping Therapy",
+    title: "Rotetry Cupping",
+    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748400613/ok1_3_et5317.webp?height=400&width=600",
+  },
+   {
+    id: 7,
+         category: "Cupping Therapy",
+    title: "Dry Cupping",
+    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397890/img_16_bfdg5p.webp?height=400&width=600",
+  },
+  {
+    id: 15,
+    category: "Baby Massage",
+    title: "New Born Baby Massage",
+    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397871/img_1_qg4vyw.jpg?height=400&width=600",
+  },
   {
     id: 1,
     category: "Muscle recovery",
@@ -11,12 +49,7 @@ export const galleryData = [
     title: "Muscle Recovery",
     image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397884/img_9_uocl3h.webp?height=400&width=600",
   },
-  {
-    id: 2,
-    category: "Deep Tissue",
-    title: "Deep Tissue Therapy Session",
-    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397888/img_13_ti9bb1.webp?height=400&width=600",
-  },
+ 
   {
     id: 3,
     category: "Deep Tissue",
@@ -61,12 +94,7 @@ export const galleryData = [
     title: "Sound Healing",
     image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397851/imgz_2_dgbd1n.webp?height=400&width=600",
   },
-  {
-    id: 10,
-    category: "Sound Healing",
-    title: "Sound Healing",
-    image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397876/img_5_jegv7k.webp?height=400&width=600",
-  },
+
  
  
  
@@ -80,7 +108,7 @@ export const galleryData = [
   {
     id: 15,
     category: "Baby Massage",
-    title: "Gentle Baby Massage",
+    title: "New Born Baby Massage",
     image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1748397871/img_1_qg4vyw.jpg?height=400&width=600",
   },
   {
@@ -103,4 +131,9 @@ export const categories = [
   "Injury Recovery",
   "Muscle recovery",
   "Facility",
+  "Auyrveda Therapy",
+  "Herbal Steam bath",
+    "Herbal Steam bath",
+    "Iastm Therapy"
+
 ]
