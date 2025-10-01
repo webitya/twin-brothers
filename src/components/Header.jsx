a6e003a35fd89d7f603c7e75818ca676f784a74f
Aditya Kumar <webitya@gmail.com>
2025-10-01 23:46:37 +0530

    Aditya

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,7 +96,7 @@ export default function Header() {
     <>
       {/* Header */}
       <header className="border-b border-slate-200 sticky top-0 z-40 bg-white/90 backdrop-blur">
-        <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
+        <div className="mx-auto px-4 py-3 flex items-center justify-between">
           {/* Logo */}
           <Link
             href="/"
