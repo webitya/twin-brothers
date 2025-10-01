8d8d8d11163a0d3dc6fe5a8d97f68d77204efe3a
Aditya Kumar <webitya@gmail.com>
2025-10-01 22:15:08 +0530

    Aditya

--- src/app/api/gallery/upload/route.js
+++ src/app/api/gallery/upload/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { uploadToGalleryBuffer } from "../../../lib/cloudinary"
+import { uploadToGalleryBuffer } from "../../../lib/db"
 
 export async function POST(req) {
   try {
